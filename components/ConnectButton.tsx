f2371c70789cfd03d4671352fd7e68bbcb9efb4b
Wubei 12 <wubshetyenguss@gmail.com>
2023-11-12 21:31:19 +0300

    a

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -3,7 +3,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function ButtonConnect() {
   return (
-    <div className='bg-[#e4e4e4] absolute text-xl text-gray-900 py-4 sm:px-32 px-28 backdrop-blur-1 hover:bg-[#e4e4e4f1] active:scale-110 transition-all'>
+    <div className='bg-[#e4e4e4] absolute text-xl text-gray-900 py-4 sm:px-32 px-28 backdrop-blur-1 hover:cursor-pointer hover:bg-[#e4e4e4f1] active:scale-110 transition-all'>
         <ConnectButton />
     </div>
   )
